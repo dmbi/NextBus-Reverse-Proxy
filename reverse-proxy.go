ae9cb388325a2a24c6ed487ad3603ee1dcef8e82
dmbi <dmbi@protonmail.com>
2017-01-15 23:40:23 +0000

    Solved Hostename Header problem

--- reverse-proxy.go
+++ reverse-proxy.go
@@ -1,16 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
 )
+
+func report(r *http.Request) {
+	r.Host = "nextbus.com"
+	r.URL.Host = r.Host
+	r.URL.Scheme = "http"
+}
 
 func main() {
 	proxy := httputil.NewSingleHostReverseProxy(&url.URL{
 		Scheme: "http",
 		Host:   "nextbus.com",
 	})
+	proxy.Director = report
 	http.Handle("/", proxy)
-	http.ListenAndServe(":8082", nil)
+	fmt.Println("Server running...")
+	http.ListenAndServe(":8080", nil)
 }
