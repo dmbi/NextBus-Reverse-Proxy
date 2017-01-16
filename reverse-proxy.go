542362d4c3b302463bdf84903d9cc0b5fabe4891
dmbi <dmbi@protonmail.com>
2017-01-16 17:56:01 +0000

    Rewrite, finally working on NextBus XML feed

--- reverse-proxy.go
+++ reverse-proxy.go
@@ -6,46 +6,32 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"net/url"
-	"strings"
 )
 
 func main() {
 	p := new(Proxy)
-	host := "webservices.nextbus.com/service/publicXMLFeed?command="
-	u, err := url.Parse(fmt.Sprintf("http://%v/", host))
+	host := "webservices.nextbus.com"
+	u, err := url.Parse(fmt.Sprintf("http://%v/api", host))
 	if err != nil {
 		log.Printf("Error parsing URL")
 	}
 
-	targetQuery := u.RawQuery
 	p.proxy = &httputil.ReverseProxy{
 		Director: func(req *http.Request) {
+			values := req.URL.Query()
+			values.Add("command", "agencyList")
+			values.Add("command", "")
 			req.Host = host
 			req.URL.Scheme = u.Scheme
 			req.URL.Host = u.Host
-			req.URL.Path = singleJoiningSlash(u.Path, req.URL.Path)
-			if targetQuery == "" || req.URL.RawQuery == "" {
-				req.URL.RawQuery = targetQuery + req.URL.RawQuery
-			} else {
-				req.URL.RawQuery = targetQuery + "&" + req.URL.RawQuery
-			}
+			req.URL.Path = "/service/publicXMLFeed"
+			req.URL.RawQuery = values.Encode()
+			log.Println(req.URL.RawQuery)
 		},
 	}
 
 	http.Handle("/", p)
 	log.Fatal(http.ListenAndServe("localhost:8080", nil))
-}
-
-func singleJoiningSlash(a, b string) string {
-	aslash := strings.HasSuffix(a, "/")
-	bslash := strings.HasPrefix(b, "/")
-	switch {
-	case aslash && bslash:
-		return a + b[1:]
-	case !aslash && !bslash:
-		return a + "/" + b
-	}
-	return a + b
 }
 
 type Proxy struct {
