68af1d45628549fe5e104c1d8054edad9d8c44fa
dmbi <dmbi@protonmail.com>
2017-01-17 18:12:26 +0000

    URL routing using gorilla/mux. Counting and timing queries

--- reverse-proxy.go
+++ reverse-proxy.go
@@ -1,43 +1,77 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"time"
+
+	"github.com/gorilla/mux"
 )
 
+var queriesCounter map[string]int
+
 func main() {
-	p := new(Proxy)
-	host := "webservices.nextbus.com"
-	u, err := url.Parse(fmt.Sprintf("http://%v/api", host))
+
+	target := "http://webservices.nextbus.com"
+	remote, err := url.Parse(target)
 	if err != nil {
-		log.Printf("Error parsing URL")
+		panic(err)
 	}
 
-	p.proxy = &httputil.ReverseProxy{
-		Director: func(req *http.Request) {
-			values := req.URL.Query()
-			values.Add("command", "agencyList")
-			values.Add("command", "")
-			req.Host = host
-			req.URL.Scheme = u.Scheme
-			req.URL.Host = u.Host
-			req.URL.Path = "/service/publicXMLFeed"
-			req.URL.RawQuery = values.Encode()
-			log.Println(req.URL.RawQuery)
-		},
+	proxy := httputil.NewSingleHostReverseProxy(remote)
+	r := mux.NewRouter()
+	r.HandleFunc("/api/{endpoint:.*}", tracker(handler(proxy), "api"))
+	r.HandleFunc("/api/stats", handler(proxy))
+	r.NotFoundHandler = http.HandlerFunc(notFound)
+	http.Handle("/", r)
+	http.ListenAndServe(":8080", r)
+}
+
+func handler(p *httputil.ReverseProxy) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		r.URL.Scheme = "http"
+		if mux.Vars(r)["endpoint"] == "stats" {
+			r.URL.Host = r.Host
+			r.URL.Path = "/api/stats"
+		} else {
+			r.Host = "webservices.nextbus.com"
+			r.URL.Host = r.Host
+			r.URL.Path = "/service/publicXMLFeed"
+			r.URL.RawQuery = "command=" + mux.Vars(r)["endpoint"]
+		}
+		p.ServeHTTP(w, r)
 	}
 
-	http.Handle("/", p)
-	log.Fatal(http.ListenAndServe("localhost:8080", nil))
 }
 
-type Proxy struct {
-	proxy *httputil.ReverseProxy
+func notFound(w http.ResponseWriter, r *http.Request) {
+	log.Println("404")
 }
 
-func (p *Proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	p.proxy.ServeHTTP(w, r)
+func timeTrack(start time.Time, endpoint string) {
+	elapsed := time.Since(start).Seconds()
+	log.Printf("[%s] request took %fs", endpoint, elapsed)
+	for k, v := range queriesCounter {
+		log.Printf("%s:%d", k, v)
+	}
+}
+
+func tracker(fn http.HandlerFunc, name string) http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		endpoint := req.URL.Path
+		defer timeTrack(time.Now(), endpoint)
+		fn(w, req)
+		if len(queriesCounter) == 0 {
+			queriesCounter = make(map[string]int)
+		}
+		if val, exists := queriesCounter[endpoint]; exists {
+			val = val + 1
+			queriesCounter[endpoint] = val
+		} else {
+			queriesCounter[endpoint] = 1
+
+		}
+	}
 }
